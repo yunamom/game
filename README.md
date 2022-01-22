4141617ea9bc0cf5c894f5af9cf6bcbcad8af6e3
yunamom <hellokin10@gmail.com>
2022-01-22 23:27:57 +0900

    보너스볼 추가 +  및 css수정 [ 최종완료 ]

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# lotto
+# JavaScript Lottery Number Generator
+
+자바스크립트 querySelector 를 사용해서 구현해보았습니다.<br>
+
+<tr>
+<img width="525" alt="Screen Shot 2022-01-22 at 3 49 44 PM" src="https://user-images.githubusercontent.com/91236026/150629189-c04d44e6-0114-4386-b866-9ee96909bc39.png">
+
--- script.js
+++ script.js
@@ -6,32 +6,41 @@ const resetButton = document.querySelector("#reset")
 
 const lottoNumbers = []
 //로또 번호를 담을 배열을 생성합니다.
-const colors = ["red","orange","yellow","green","blue","purple"]
+const colors = ["red","orange","yellow","green","blue","purple","clear","pink"]
 //로또 볼에 색을 더해줄수있는 배열을 생성합니다.
 
-function paintNumber(number,){
-    const i = lottoNumbers.length
+
+function paintNumber(number){
+  const i = lottoNumbers.length;
+  setTimeout(function(){
     const eachNumDiv = document.createElement("div")
     eachNumDiv.classList.add('eachnum')
     eachNumDiv.style.backgroundColor = colors[i-1]
     //색상을 입혀줍니다.
     eachNumDiv.textContent = number
-    numbersDiv.appendChild(eachNumDiv)
+    numbersDiv.appendChild(eachNumDiv) 
+  },i*550);//볼이 하나씩 나올수있게 Delay 를 해준다. 
 }
 drawButton.addEventListener("click",function(){
   //  console.log("!") //화면에 잘나오는지 확인하기.
-  while(lottoNumbers.length < 6){
+  while(lottoNumbers.length < 8){
       var num=Math.floor(Math.random()*45)+1;
       if(lottoNumbers.indexOf(num) === -1){ 
         //중복되는 숫자가 없을시에 값을 push 해줍니다.
+        if(lottoNumbers.length==6){
+          num = "+"; //보너스ball 앞에 넣어준다.
+        }
         lottoNumbers.push(num)
-        paintNumber(num)
-      }
-    }
+        paintNumber(num);
+      } 
+  }
    // console.log(lottoNumbers) //colsole 에서 확인하기.
 })
-resetButton.addEventListener("click",function(){
-    lottoNumbers.splice(0,6)
-    numbersDiv.innerHTML = ""
+resetButton.addEventListener("click",function(){ 
+  lottoNumbers.splice(0,8);
+  numbersDiv.innerHTML = "";
   //reset 버튼을 누르면 numbers 안에 값을 비워준다.
+  history.go(0);
+  /*lotto ball 이 생성되는 중간에 reset 버튼을 누르면 그뒤에 담겨있던 나머지 볼들이 계속나와서
+  history.go 를 추가해주었다 페이시 새로고침이되서 처음값으로 돌아간다.*/
 })
