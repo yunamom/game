ecfea252e6eebe55d931fe1b34eeb2c2a6b871c5
yunamom <hellokin10@gmail.com>
2022-01-22 13:29:31 +0900

    랜덤번호 생성

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ const lottoNumbers = []
 //로또 번호를 담을 배열을 생성합니다.
 const colors = ["red","orange","yellow","green","blue"]
 //로또 볼에 색을 더해줄수있는 배열을 생성합니다.
+
+function paintNumber(number){
+    const eachNumDiv = document.createElement("div")
+    eachNumDiv.classList.add('eachnum')
+    eachNumDiv.textContent = number
+    numbersDiv.appendChild(eachNumDiv)
+}
 drawButton.addEventListener("click",function(){
-    console.log("!") //화면에 잘나오는지 확인하기.
+  //  console.log("!") //화면에 잘나오는지 확인하기.
+  while(lottoNumbers.length < 6){
+      var num=Math.floor(Math.random()*45)+1;
+      if(lottoNumbers.indexOf(num) === -1){ 
+        //중복되는 숫자가 없을시에 값을 push 해줍니다.
+        lottoNumbers.push(num)
+        paintNumber(num)
+      }
+    }
+   // console.log(lottoNumbers) //colsole 에서 확인하기.
 })
